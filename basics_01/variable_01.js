f3ee93003d0af6327753da5e53b2a55f9f3f47c0
chekitmarpak <60998865+chekitmarpak@users.noreply.github.com>
2025-07-06 13:24:48 +0000

    uploaded the example JS file containing var, let, const and console.table;

---
+++ basics_01/variable_01.js
@@ -0,0 +1,12 @@
+const accountId = "AC123456"
+let accountEmail = "chekit@gmail.com"
+var accountPassword = "123456"
+accountCity = "Wani"
+let accountState
+
+accountEmail = "cm@fmail.com"
+accountPassword = "124321"
+accountCity = "Chandrapur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountId + " " + accountEmail +  " " + accountPassword +  " " + accountCity)
