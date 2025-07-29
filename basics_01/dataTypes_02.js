93a913f8e26cf402147abf01eedb9412a96c5524
chekitmarpak <60998865+chekitmarpak@users.noreply.github.com>
2025-07-29 01:20:24 +0000

    datatypes tutorial

---
+++ basics_01/dataTypes_02.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+let name = "Hi"
+let number = 4
+let checkIf = false
+let empty;
+
+//number => 2 to power 52
+//bigInt
+//string => ""
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+console.log(typeof "Chekit");
+
+console.log(name)
