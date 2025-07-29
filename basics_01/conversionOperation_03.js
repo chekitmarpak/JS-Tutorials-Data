1dbe0da402abdbb29ef3a009404810f421c19e10
chekitmarpak <60998865+chekitmarpak@users.noreply.github.com>
2025-07-29 02:23:08 +0000

    conversion of Data Types String to number boolean to String

---
+++ basics_01/conversionOperation_03.js
@@ -0,0 +1,10 @@
+let score = true
+console.log(score);
+
+let newVar = Number(score);
+console.log(typeof score);
+console.log(score + " " + newVar);
+
+console.log(typeof newVar);
+// 30 -> number
+// 
